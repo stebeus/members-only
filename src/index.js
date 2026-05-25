46074d9d375bc471c60ce050256d1827eaf2278e
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-25 12:45:30 -0300

    refactor!: improve structure

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { join } from 'node:path';
+
+import express from 'express';
+import passport from 'passport';
+
+import { PORT } from './config.js';
+import { handleError, handleNotFoundError } from './controllers/errors.js';
+import { expressSession } from './lib/express-session.js';
+import { logger, pino } from './lib/pino-http.js';
+import { index } from './routes/index.js';
+
+const app = express();
+
+const { dirname } = import.meta;
+const viewsPath = join(dirname, 'views');
+
+app.set('views', viewsPath);
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(expressSession);
+app.use(passport.session());
+app.use(pino);
+
+app.use(index);
+
+app.use(handleNotFoundError);
+app.use(handleError);
+
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
