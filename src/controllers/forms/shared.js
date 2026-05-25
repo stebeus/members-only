86c3f4eb18d7046034e04ceb735faebbe0d25408
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-25 15:46:09 -0300

    feat: add `renderForm`

--- src/controllers/forms/shared.js
+++ src/controllers/forms/shared.js
@@ -1,3 +1,20 @@
 const props = { autocomplete: 'on', isRequired: true };
 
-export { props };
+const renderForm = (
+	res,
+	{ title, action, errs, inputs = [], hasTextarea, textarea, submissionLabel },
+) => {
+	const status = errs == null ? 201 : 400;
+
+	return res.status(status).render('form', {
+		title,
+		action,
+		errs: errs?.messages,
+		inputs,
+		hasTextarea,
+		textarea,
+		submissionLabel,
+	});
+};
+
+export { props, renderForm };
