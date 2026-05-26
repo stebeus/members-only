8b133cec6005cef4c490d267c75ec6ec4112084f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-26 17:07:09 -0300

    feat: add forms route

---
+++ src/routes/forms.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+
+import {
+	getLogIn,
+	logInAuthentication,
+} from '#root/controllers/forms/log-in.js';
+import { getSignUp, signUp } from '#root/controllers/forms/sign-up.js';
+
+const router = Router();
+
+router.get('/sign-up', getSignUp);
+router.get('/log-in', getLogIn);
+
+router.post('/sign-up', signUp);
+router.post('/log-in', logInAuthentication);
+
+export { router as forms };
