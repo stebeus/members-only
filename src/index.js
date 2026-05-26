5f4d7ac2f2c7cf66a51356f99660a07dfacbfeab
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-26 17:13:15 -0300

    refactor: use backend app template structure

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import { PORT } from './config.js';
 import { handleError, handleNotFoundError } from './controllers/errors.js';
 import { expressSession } from './lib/express-session.js';
 import { logger, pino } from './lib/pino.js';
+import { forms } from './routes/forms.js';
 import { index } from './routes/index.js';
 
-import './middleware/auth.js';
-
 const app = express();
+
+// Setup
 
 const { dirname } = import.meta;
 const viewsPath = join(dirname, 'views');
@@ -19,15 +20,22 @@ const viewsPath = join(dirname, 'views');
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(expressSession);
-app.use(passport.session());
 app.use(pino);
 
+app.use(expressSession);
+app.use(passport.session());
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// App
+
 app.use(index);
+app.use(forms);
 
 app.use(handleNotFoundError);
 app.use(handleError);
+
+// Listener
 
 app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
