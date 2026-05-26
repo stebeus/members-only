a5dd00bc34116ab0dbf5ada0ee08da1bfa93779f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-26 17:12:55 -0300

    feat: store session in PostgreSQL

--- src/lib/express-session.js
+++ src/lib/express-session.js
@@ -1,9 +1,17 @@
+import connectPgSimple from 'connect-pg-simple';
 import session from 'express-session';
 
 import { SESSION_SECRET } from '#root/config.js';
+
+import { sql } from './postgres.js';
+
+const PostgresSession = connectPgSimple(session);
 
 export const expressSession = session({
 	resave: false,
 	saveUninitialized: false,
 	secret: SESSION_SECRET,
+	store: new PostgresSession({
+		pool: sql,
+	}),
 });
