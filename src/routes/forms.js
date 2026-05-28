fdc8e85362678984469170c4275adb600b892a73
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-28 09:26:59 -0300

    feat: add join-the-club routes

--- src/routes/forms.js
+++ src/routes/forms.js
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import {
+	getJoinTheClub,
+	joinTheClub,
+} from '#root/controllers/forms/join-the-club/handler.js';
 import { getLogIn, logInAuth } from '#root/controllers/forms/log-in.js';
 import { getSignUp, signUp } from '#root/controllers/forms/sign-up/handler.js';
 
@@ -7,8 +11,10 @@ const router = Router();
 
 router.get('/sign-up', getSignUp);
 router.get('/log-in', getLogIn);
+router.get('/join-the-club', getJoinTheClub);
 
 router.post('/sign-up', signUp);
 router.post('/log-in', logInAuth);
+router.post('/join-the-club', joinTheClub);
 
 export { router as forms };
