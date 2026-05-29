01460d02ea2c6b6be6a75c8843096bf9ac96511a
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-29 15:22:33 -0300

    refactor!: improve log-in controller

--- src/controllers/forms/log-in.js
+++ src/controllers/forms/log-in.js
@@ -1,38 +1,38 @@
 import passport from 'passport';
 
-import { props, renderForm } from './form.js';
+import { maxChar } from '#root/config/constants.js';
+
+import { props, renderForm } from './render.js';
 
 const inputs = [
 	{
 		label: 'Username',
 		name: 'username',
-		maxLength: 25,
+		maxLength: maxChar.user.username,
 		...props,
 	},
 	{
 		label: 'Password',
 		name: 'password',
 		type: 'password',
+		maxLength: maxChar.user.default,
 		...props,
 	},
 ];
 
-const renderLogIn = (res, errs) => {
+const getLogIn = (req, res) =>
 	renderForm(res, {
 		title: 'Log in',
 		action: '/log-in',
-		errs,
+		errs: req.session.messages,
 		inputs,
 		submissionLabel: 'Log in',
 	});
-};
 
-const getLogIn = (req, res) => renderLogIn(res, req.session.messages);
-
-const logInAuth = passport.authenticate('local', {
+const logIn = passport.authenticate('local', {
 	successRedirect: '/',
 	failureRedirect: '/log-in',
 	failureMessage: 'Incorrect username or password',
 });
 
-export { getLogIn, logInAuth };
+export { getLogIn, logIn };
