53287afaf470449c93ec1cc5696341d74eea04bc
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-29 15:22:15 -0300

    refactor!: improve sign-up controller

--- src/controllers/forms/sign-up/handler.js
+++ src/controllers/forms/sign-up/handler.js
@@ -1,8 +1,9 @@
 import { hash } from 'bcryptjs';
 import { matchedData, validationResult } from 'express-validator';
 
-import { props, renderForm } from '#root/controllers/forms/form.js';
-import * as model from '#root/models/model.js';
+import { maxChar } from '#root/config/constants.js';
+import { props, renderForm } from '#root/controllers/forms/render.js';
+import * as userModel from '#root/models/users.js';
 
 import { validations } from './validations.js';
 
@@ -11,31 +12,33 @@ const inputs = [
 		label: 'Full name',
 		name: 'fullName',
 		placeholder: 'John Doe',
-		maxLength: 100,
+		maxLength: maxChar.user.default,
 		...props,
 	},
 	{
 		label: 'Username',
 		name: 'username',
 		placeholder: 'john_doe123',
-		maxLength: 25,
+		maxLength: maxChar.user.username,
 		...props,
 	},
 	{
 		label: 'Password',
 		name: 'password',
 		type: 'password',
+		maxLength: maxChar.user.default,
 		...props,
 	},
 	{
 		label: 'Confirm password',
 		name: 'confirmPassword',
 		type: 'password',
+		maxLength: maxChar.user.default,
 		...props,
 	},
 ];
 
-const renderSignUp = (res, errs) =>
+const render = (res, errs) =>
 	renderForm(res, {
 		title: 'Sign up',
 		action: '/sign-up',
@@ -44,18 +47,18 @@ const renderSignUp = (res, errs) =>
 		submissionLabel: 'Sign up',
 	});
 
-const getSignUp = (_req, res) => renderSignUp(res);
+const getSignUp = (_req, res) => render(res);
 
 const signUp = [
 	validations,
 	async (req, res) => {
 		const errs = validationResult(req);
-		if (!errs.isEmpty()) return renderSignUp(res, errs.array());
+		if (!errs.isEmpty()) return render(res, errs.array());
 
 		const { fullName, username, password } = matchedData(req);
 		const hashedPassword = await hash(password, 10);
 
-		await model.createUser(fullName, username, hashedPassword);
+		await userModel.createUser(fullName, username, hashedPassword);
 
 		res.redirect('/log-in');
 	},
---
+++ src/controllers/forms/sign-up/validators.js
@@ -0,0 +1,44 @@
+import { body } from 'express-validator';
+
+import { createLengthChain } from '#root/controllers/forms/validators.js';
+
+const createNameChain = ({
+	fieldName,
+	fieldLabel = fieldName,
+	charFn = 'isAlpha',
+	ignore,
+	min,
+	max,
+}) => {
+	const charConstraint = charFn === 'isAlpha' ? 'alphabetic' : 'alphanumeric';
+
+	return createLengthChain({ fieldName, fieldLabel, min, max })
+		[charFn]('en-US', { ignore })
+		.withMessage(
+			`${fieldLabel} must contain only ${charConstraint} characters`,
+		);
+};
+
+const fullName = createNameChain({
+	fieldName: 'fullName',
+	fieldLabel: 'full name',
+	ignore: ' ',
+	max: 100,
+});
+
+const username = createNameChain({
+	fieldName: 'username',
+	charFn: 'isAlphanumeric',
+	ignore: '_',
+	max: 25,
+});
+
+const password = createLengthChain({ fieldName: 'password', max: 100 });
+
+const matchPasswords = (value, { req }) => value === req.body.password;
+
+const passwordConfirmation = body('passwordConfirmation')
+	.custom(matchPasswords)
+	.withMessage('Passwords must match');
+
+export const validations = [fullName, username, password, passwordConfirmation];
