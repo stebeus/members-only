2869f5de9b2309862e3d0f795e0942a1f96042f1
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-29 15:23:21 -0300

    refactor!: improve forms route

--- src/routes/forms.js
+++ src/routes/forms.js
@@ -7,22 +7,22 @@ import {
 } from '#root/controllers/forms/create-post/handler.js';
 import {
 	getJoinTheClub,
-	joinTheClub,
+	joinClub,
 } from '#root/controllers/forms/join-the-club/handler.js';
-import { getLogIn, logInAuth } from '#root/controllers/forms/log-in.js';
+import { getLogIn, logIn } from '#root/controllers/forms/log-in.js';
 import { getSignUp, signUp } from '#root/controllers/forms/sign-up/handler.js';
 
 const router = Router();
 
-router.get('/sign-up', getSignUp);
-router.get('/log-in', getLogIn);
 router.get('/create-post', getCreatePost);
 router.get('/delete-post/:postId', deletePost);
+router.get('/log-in', getLogIn);
 router.get('/join-the-club', getJoinTheClub);
+router.get('/sign-up', getSignUp);
 
-router.post('/sign-up', signUp);
-router.post('/log-in', logInAuth);
 router.post('/create-post', createPost);
-router.post('/join-the-club', joinTheClub);
+router.post('/join-the-club', joinClub);
+router.post('/log-in', logIn);
+router.post('/sign-up', signUp);
 
 export { router as forms };
