0c86a415e81821e3748a2665078ca173ad4fc94b
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-29 14:54:55 -0300

    refactor: improve timestamp usage for better readability

--- src/models/schema.js
+++ src/models/schema.js
@@ -3,6 +3,8 @@ const timestamps = [
 	'updated_at TIMESTAMPTZ',
 ];
 
+const [updatedAt] = timestamps;
+
 export const schema = `
 	CREATE TABLE IF NOT EXISTS users (
 		id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -11,7 +13,7 @@ export const schema = `
 		password TEXT NOT NULL,
 		is_member BOOLEAN NOT NULL,
 		is_admin BOOLEAN NOT NULL,
-		${timestamps.join(', ')}
+		${timestamps}
 	);
 
 	CREATE TABLE IF NOT EXISTS posts (
@@ -19,6 +21,6 @@ export const schema = `
 		user_id BIGINT REFERENCES users (id),
 		title VARCHAR (50) NOT NULL,
 		content VARCHAR(500) NOT NULL,
-		${timestamps[0]}
+		${updatedAt}
 	);
 `;
