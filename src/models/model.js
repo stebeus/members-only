ac70fc9b669ccb8caf8dfabff423f52f6350558f
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-29 08:10:58 -0300

    feat: add `deletePost`

--- src/models/model.js
+++ src/models/model.js
@@ -42,9 +42,13 @@ const getAllPosts = async () => {
 	return rows;
 };
 
+const deletePost = async (id) =>
+	await query('DELETE FROM posts WHERE id = $1', [id]);
+
 export {
 	createPost,
 	createUser,
+	deletePost,
 	getAllPosts,
 	getUserById,
 	getUserByUsername,
