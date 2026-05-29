57fb03b841fc01d7c8b2dfcad06cb05644552a04
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-29 09:18:45 -0300

    feat: query posts with users

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,9 @@
-const getIndex = (req, res) => res.render('index');
+import { getAllPostsWithUsers } from '#root/models/model.js';
+
+const getIndex = async (req, res) => {
+	const posts = await getAllPostsWithUsers();
+	res.render('index', { posts });
+};
 
 const logOut = (req, res, next) => {
 	req.logout((err) => err != null && next(err));
