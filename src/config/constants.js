397ebe92d1c772871973e05360281b2749eebb5d
stebeus <189894010+stebeus@users.noreply.github.com>
2026-05-29 15:19:32 -0300

    feat: add constants config

---
+++ src/config/constants.js
@@ -0,0 +1,17 @@
+const maxChar = {
+	user: {
+		default: 100,
+		username: 25,
+	},
+	post: {
+		title: 50,
+		content: 500,
+	},
+};
+
+const secretCode = {
+	admin: 'Los Angeles: Critical Mass',
+	club: 'The Odin Project',
+};
+
+export { maxChar, secretCode };
